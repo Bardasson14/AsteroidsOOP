48cbce1b6c10a51c505b79cc01ec0a8d9b1d6f57
laaapf <lucaspfonseca98@gmail.com>
2019-06-08 23:22:09 -0300

    .

---
+++ core/src/com/mygdx/asteroids/AsteroidsGame.java
@@ -0,0 +1,34 @@
+package com.mygdx.asteroids;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.screens.MainGameScreen;
+
+public class AsteroidsGame extends Game {
+
+	public SpriteBatch batch;
+
+	@Override
+	public void create() {
+
+		batch = new SpriteBatch();
+		this.setScreen(new MainGameScreen(this));
+
+	}
+
+	// Player player = new Player(50, 50);
+
+	@Override
+	public void render() {
+		super.render();
+	}
+
+}
+
+
+
+
