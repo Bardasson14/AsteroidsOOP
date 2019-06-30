dff3021c0af3e299e904169ba9cba8ef906f7dbf
Vitor Bardasson <vbardasson@id.uff.br>
2019-06-30 14:35:46 -0300

    Import corrigido

--- core/src/com/mygdx/screens/MainGameScreen.java
+++ core/src/com/mygdx/screens/MainGameScreen.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.Game;
 //import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
